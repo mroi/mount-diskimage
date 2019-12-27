3f8f49bb20753338be05c4ab9a1dfc81300bf5ab
Michael Roitzsch <reactorcontrol@icloud.com>
2019-12-27 15:15:15 +0100

    image compaction

--- main.swift
+++ main.swift
@@ -11,11 +11,26 @@ let imageMounts = [
 switch CommandLine.argc {
 case 1:
 	print(imageMounts.keys.joined(separator: "\n"))
+
 case 2:
+	let hdiutil = URL(fileURLWithPath: "/usr/bin/hdiutil")
 	guard let image = imageMounts[CommandLine.arguments[1]] else {
 		exit(EX_USAGE)
 	}
-	os_log("asked to mount %{public}s", image)
+
+	// compact the disk image for about 10% of mount attempts
+	if Float.random(in: 0...1) < 0.1 {
+		guard let process = try? Process.run(hdiutil, arguments: ["compact", image]) else {
+			exit(EX_OSERR)
+		}
+		process.waitUntilExit()
+		if process.terminationStatus != EX_OK {
+			os_log("compaction failed for disk image ‘%{public}s’", image)
+		} else {
+			os_log("compacted disk image ‘%{public}s’", image)
+		}
+	}
+
 default:
 	exit(EX_USAGE)
 }
